9796daed2f095c3ff5a4958aa2d45a8a7b71f9eb
Hat-Trick Patrick <5185596+hattrickpatrick@users.noreply.github.com>
2020-09-12 14:51:23 -0400

    ConVar changes
    Removed all ConVars.
    SQL storage of gamemode variables in sv.db (table 'prop_hunt_vars'). All values are stored as strings.
    Helper functions added to PHX object for retrieving values of the vars in boolean, string, and number formats.
    Converted CVAR references to new GetVar calls.

--- gamemodes/prop_hunt/gamemode/sh_init.lua
+++ gamemodes/prop_hunt/gamemode/sh_init.lua
@@ -6,117 +6,131 @@ PHX.ConfigPath 	= "phx_data"
 PHX.VERSION		= "X"
 PHX.REVISION	= "16.08.20" --Format: dd/mm/yy.
 
--- Convars.
--- WARNING: 
--- THIS CONVAR SECTIONS WILL BE CHANGED IN FUTURE. 
--- DUE TO REPLICATION PROBLEMS, ALL CONVAR OBJECTS ON CLIENTSIDE WILL USE NEW METHOD TO "GETGLOBAL*" FUNCTION INSTEAD OF GETTING A VALUE FROM CONVAR OBJECT.
-PHX.CVAR = PHX.CVAR or {}
-local CVAR_SERVER_ONLY = { FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_NOTIFY }
-local CVAR_SERVER_ONLY_NO_ARCHIVE = { FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_NOTIFY }
-local CVAR_SERVER_ONLY_NO_NOTIFY = { FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED }
-local CVAR_SERVER_HIDDEN = { FCVAR_SERVER_CAN_EXECUTE, 0x10, FCVAR_DONTRECORD }
+--[[------------------------------------------------
+    Complete overhaul and replacement of ConVars in
+    favor of client/server networked globals and
+    storage in sv.db (sql library)
 
--- Language
-PHX.CVAR.UseForceLang	= CreateConVar("ph_use_lang", "0", CVAR_SERVER_ONLY, "Enable forced-language display. THIS WILL BYPASS USER-PREFERED LANGUAGE!", 0, 1)
-PHX.CVAR.ForcedLanguage	= CreateConVar("ph_force_lang", "en_us", CVAR_SERVER_ONLY, "Default language to force.")
-PHX.CVAR.DefaultLang	= CreateConVar("ph_default_lang", "en_us", CVAR_SERVER_ONLY, "This is their first time join to set which language should be set. You should always leave this as 'en_us'.")
+    @author HatTrickPatrick
+------------------------------------------------]]--
 
-if CLIENT then
+-- Default Configuration
+PHX.DEFAULT_VARS = {
+    ["UseForceLang"] = "0",
+    ["ForcedLanguage"] = "en_us",
+    ["DefaultLang"] = "en_us",
+    ["UseCustomMdlProp"] = "0",
+    ["UseCustomMdlHunter"] = "0",
+    ["UseModelType"] = "0",
+    ["AutoTauntEnable"] = "1",
+    ["AutoTauntDelay"] = "45",
+    ["CustomTauntMode"] = "0",
+    ["CustomTauntDelay"] = "6",
+    ["NormalTauntDelay"] = "2",
+    ["PropJumpPower"] = "1.4",
+    ["PropNotifyRotation"] = "1",
+    ["FreezeCamera"] = "1",
+    ["FreezeCamUseSingle"] = "0",
+    ["FreezeCamCue"] = "misc/freeze_cam.wav",
+    ["NotifyPlayerJoinLeaves"] = "1",
+    ["UseLuckyBall"] = "1",
+    ["UseDevilCrystal"] = "1",
+    ["HunterPenalty"] = "5",
+    ["HunterKillBonus"] = "100",
+    ["GameTime"] = "30",
+    ["BlindTime"] = "30",
+    ["RoundTime"] = "300",
+    ["TotalRounds"] = "10",
+    ["WaitForPlayers"] = "1",
+    ["MinWaitForPlayers"] = "1",
+    ["EnableOBBMod"] = "1",
+    ["ApplyOBBonRound"] = "1",
+    ["UseNewMKBren"] = "1",
+    ["CheckSpace"] = "0",
+    ["SeePlayerNames"] = "0",
+    ["CameraCollision"] = "0",
+    ["PropCollide"] = "0",
+    ["HLACombine"] = "1",
+    ["SwapTeam"] = "1",
+    ["ChangeTeamLimit"] = "8",
+    ["EnableTeamBalance"] = "1",
+    ["EnableNewChat"] = "0",
+    ["NewChatPosSubtract"] = "50",
+    ["AllowRespawnOnBlind"] = "1",
+    ["AllowRespawnOnBlindTeam"] = "0",
+    ["AllowSpectatorRespawnOnBlind"] = "1",
+    ["BlindRespawnTimePercent"] = "0.75",
+    ["AllowPickupProp"] = "3",
+    ["UseCustomMapVote"] = "0",
+    ["CustomMapVoteCall"] = "MapVote.Start()",
+    ["ForceJoinBalancedTeams"] = "0",
+    ["SMGGrenadeCounts"] = "1",
+    ["BeVerbose"] = "0",
+    ["AllowRespawnOnBlindBetweenTeams"] = "0",
+}
 
-	PHX.CVAR.Language = CreateClientConVar("ph_cl_language", PHX.CVAR.DefaultLang:GetString(), true, true, "Prefered language to use")
-	
-	PHX.CVAR.DefaultTauntKey		= CreateClientConVar("ph_default_taunt_key", KEY_F3, true, true, "Default random taunt key to be used. Default is F3 ("..tostring(KEY_F3)..")")
-	PHX.CVAR.DefaultCustomTauntKey	= CreateClientConVar("ph_default_customtaunt_key", KEY_C, true, true, "Default custom taunt key to be used. Default is C ("..tostring(KEY_C)..")")
-	PHX.CVAR.RotationLockKey		= CreateClientConVar("ph_default_rotation_lock_key", KEY_R, true, true, "Default Rotation lock key to be used. Default is R ("..tostring(KEY_R)..")")
+-- Query SQL for var data
+-- If the data isn't in the SQL db yet, we create the table and
+-- insert the default values
+-- Once they're read in, create global strings of the values
 
-end
-
-PHX.CVAR.UseCustomMdlProp	= CreateConVar("ph_use_custom_plmodel_for_prop", "0", CVAR_SERVER_ONLY, "Should use a custom Player's Model for Props when the round begins?", 0, 1)
-PHX.CVAR.UseCustomModel 	= CreateConVar("ph_use_custom_plmodel", "0", CVAR_SERVER_ONLY, "Should use a custom player model available for Hunters?\nPlease note that you must have to activate \'ph_use_custom_plmodel_for_prop\' too!", 0, 1)
-PHX.CVAR.UseModelType		= CreateConVar("ph_use_playermodeltype", "0", CVAR_SERVER_ONLY, "Which model list that should deliver from? 0 = All Playermodels availale, 1 = Use Legacy method: list.Get('PlayerOptionsModel') (Recommended if you want to custom your model list)", 0, 1)
-
-PHX.CVAR.AutoTauntEnable	= CreateConVar("ph_autotaunt_enabled", "1",CVAR_SERVER_ONLY_NO_NOTIFY, "Should auto taunting be enabled", 0, 1)
-PHX.CVAR.AutoTauntDelay		= CreateConVar("ph_autotaunt_delay", "45", CVAR_SERVER_ONLY_NO_NOTIFY, "The delay for the auto taunt")
-PHX.CVAR.CustomTauntMode	= CreateConVar("ph_custom_taunt_mode", "0", CVAR_SERVER_ONLY, "Enable custom taunts for prop teams by pressing C? (Default 0)\n  You must have a list of custom taunts to enable this.", 0, 2)
-PHX.CVAR.CustomTauntDelay	= CreateConVar("ph_customtaunts_delay", "6", CVAR_SERVER_ONLY, "How many in seconds delay for props to play custom taunt again? (Default is 6)")
-PHX.CVAR.NormalTauntDelay	= CreateConVar("ph_normal_taunt_delay", "2", CVAR_SERVER_ONLY, "How many in seconds delay for props to play normal [F3] taunt again? (Default is 2)")
-
-PHX.CVAR.PropJumpPower		= CreateConVar("ph_prop_jumppower", "1.4", CVAR_SERVER_ONLY, "Multipliers for Prop Jump Power (Do not confused with Prop's Gravity!). Default is 1.4. Min. 1.")
-PHX.CVAR.PropNotifyRotation	= CreateConVar("ph_notice_prop_rotation", "1", CVAR_SERVER_ONLY, "Enable Prop Rotation notification on every time Prop Spawns.", 0, 1)
-
-PHX.CVAR.FreezeCamera		= CreateConVar("ph_freezecam", "1", CVAR_SERVER_ONLY, "Freeze Camera.")
-PHX.CVAR.FreezeCamUseSingle	= CreateConVar("ph_fc_use_single_sound", "0", CVAR_SERVER_ONLY, "Use single Freezecam sound instead of sound list?")
-PHX.CVAR.FreezeCamCue		= CreateConVar("ph_fc_cue_path", "misc/freeze_cam.wav", CVAR_SERVER_ONLY, "Path for single Freezecam sound.")
-
-PHX.CVAR.NotifyPlayerJoinLeaves = CreateConVar("ph_notify_player_join_leave", "1", CVAR_SERVER_ONLY, "Notify Player Join and Leave in the Chat?")
-
-PHX.LegalSoundPath			= PHX.CVAR.FreezeCamCue:GetString() or "misc/freeze_cam.wav"
 if SERVER then
-	cvars.AddChangeCallback("ph_fc_cue_path", function(cvar,old,new)
-		local ReplaceIllegalPath = string.Replace(new, "\\", "/")
-		print("Warning: Detected Backslash (\\) characters. Please use \"/\" instead.")
-		RunConsoleCommand( "ph_fc_cue_path", ReplaceIllegalPath )
-		PHX.LegalSoundPath = ReplaceIllegalPath
-	end, "fc_path_modify")
+    PHX.VARS = {}
+
+    if sql.TableExists("prop_hunt_vars") then
+        local datas = sql.Query("SELECT * FROM prop_hunt_vars")
+        
+        if datas then
+            for _, row in pairs(datas) do
+                PHX.VARS[row.key] = row.value
+            end
+        else
+            print("[PHX] WARNING: Failed to load vars/settings from prop_hunt_vars in SQL DB. Using the default config as a failsafe! Please check your sv.db for issues")
+            print("[PHX] The SQL error was: " .. sql.LastError() or "(unavailable)")
+            PHX.VARS = table.Copy(PHX.DEFAULT_VARS)
+        end
+    else
+        print("[PHX] NOTICE: Table prop_hunt_vars not found in SQL DB. Attempting to create it.")
+        if sql.Query("CREATE TABLE prop_hunt_vars (key TEXT NOT NULL PRIMARY KEY, value TEXT)") then
+            print("[PHX] NOTICE: Table prop_hunt_vars created in SQL DB. Attempting to setup default config.")
+            for k,v in SortedPairs(PHX.DEFAULT_VARS) do
+                if sql.Query(string.format("INSERT INTO prop_hunt_vars(name, value) VALUES('%s', '%s')", k, v)) then
+                    print("[PHX] Added default config '" .. k .. "'='" .. v .. "' to prop_hunt_vars")
+                else
+                    print("[PHX] ERROR: Failed to insert default config '" .. k .. "'='" .. v .. " to prop_hunt_vars")
+                    print("[PHX] The SQL error was: " .. sql.LastError())
+                end
+            end
+        else
+            print("[PHX] ERROR: Failed to create table prop_hunt_vars")
+            print("[PHX] The SQL error was: " .. sql.LastError())
+        end
+        
+        PHX.VARS = table.Copy(PHX.DEFAULT_VARS)
+    end
+    
+    for k,v in pairs(PHX.VARS) do
+        SetGlobalString("phvar_"..k, v)
+    end
 end
 
-PHX.CVAR.UseLuckyBall		= CreateConVar("ph_enable_lucky_balls", "1", CVAR_SERVER_ONLY_NO_ARCHIVE, "Spawn Lucky balls on breakable props?", 0, 1)
-PHX.CVAR.UseDevilCrystal	= CreateConVar("ph_enable_devil_balls", "1", CVAR_SERVER_ONLY_NO_ARCHIVE, "Spawn Devil balls when hunter dies?", 0, 1)
+function PHX.GetVarBool(n)
+    return GetGlobalString(n, PHX.DEFAULT_VARS[n]) != "0"
+end
+function PHX.GetVarNum(n)
+    return tonumber(GetGlobalString(n, PHX.DEFAULT_VARS[n]))
+end
+function PHX.GetVarString(n)
+    return tostring(GetGlobalString(n, PHX.DEFAULT_VARS[n]))
+end
 
-PHX.CVAR.HunterPenalty		= CreateConVar("ph_hunter_fire_penalty", "5", CVAR_SERVER_ONLY, "Health points removed from hunters when they shoot.", 1, 500)
-PHX.CVAR.HunterKillBonus	= CreateConVar("ph_hunter_kill_bonus", "100", CVAR_SERVER_ONLY, "How much health to give back to the Hunter after killing a prop.", 0, 500)
-PHX.CVAR.GameTime			= CreateConVar("ph_game_time", "30", CVAR_SERVER_ONLY, "Maximum Time Left (in minutes) - Default is 30 minutes.")
-PHX.CVAR.BlindTime			= CreateConVar("ph_hunter_blindlock_time", "30", CVAR_SERVER_ONLY, "How long hunters are blinded (in seconds)")
-PHX.CVAR.RoundTime			= CreateConVar("ph_round_time", "300", CVAR_SERVER_ONLY, "Time (in seconds) for each rounds.")
-PHX.CVAR.TotalRounds		= CreateConVar("ph_rounds_per_map", "10", CVAR_SERVER_ONLY, "Numbers played on a map (Default: 10)")
-PHX.CVAR.WaitForPlayers		= CreateConVar("ph_waitforplayers", "1", CVAR_SERVER_ONLY, "Should we wait for players for proper round?", 0, 1 )
-PHX.CVAR.MinWaitForPlayers	= CreateConVar("ph_min_waitforplayers", "1", CVAR_SERVER_ONLY_NO_NOTIFY, "Numbers of mininum players that we should wait for round start. Value must not contain less than 1.", 1, game.MaxPlayers())
 
-PHX.CVAR.EnableOBBMod		= CreateConVar("ph_sv_enable_obb_modifier", "1",CVAR_SERVER_ONLY_NO_NOTIFY, "Developer: Enable OBB Model Data Modifier", 0, 1)
-PHX.CVAR.ApplyOBBonRound	= CreateConVar("ph_reload_obb_setting_everyround", "1",CVAR_SERVER_ONLY_NO_NOTIFY, "Developer: Reload OBB Model Data Modifier Every round Restarts", 0, 1)
 
-PHX.CVAR.UseNewMKBren		= CreateConVar("ph_mkbren_use_new_mdl", "1", CVAR_SERVER_ONLY_NO_NOTIFY, "Use new model for Bren MK II Bonus Weapon (Require Map Restart!!)", 0, 1)
-PHX.CVAR.CheckSpace			= CreateConVar("ph_check_props_boundaries", "0", CVAR_SERVER_ONLY, "Enable prop boundaries/room check? This will prevent you to get stuck with other objects (such as prop, wall, etc)", 0, 1)
-PHX.CVAR.SeePlayerNames		= CreateConVar("ph_enable_plnames", "0", CVAR_SERVER_ONLY, "Serverside control for if a clients see client\'s team player names through walls.", 0, 1)
-PHX.CVAR.CameraCollision	= CreateConVar("ph_prop_camera_collisions", "0", CVAR_SERVER_ONLY, "Attempts to stop props from viewing inside walls.", 0, 1)
-PHX.CVAR.PropCollide		= CreateConVar("ph_prop_collision", "0", CVAR_SERVER_ONLY, "Should Team Props collide with each other?", 0, 1)
-PHX.CVAR.HLACombine			= CreateConVar("ph_add_hla_combine", "1", CVAR_SERVER_ONLY, "Add HLA Combine to default combine models? [REQUIRE MAP RESTART!]", 0, 1)
-PHX.CVAR.SwapTeam			= CreateConVar("ph_swap_teams_every_round", "1", CVAR_SERVER_ONLY, "Should teams swapped on every round?", 0, 1)
-PHX.CVAR.ChangeTeamLimit	= CreateConVar("ph_max_teamchange_limit", "8", CVAR_SERVER_ONLY_NO_NOTIFY, "Define how many times player can assign to their team. Default is 5. -1 means disable it.")
-PHX.CVAR.EnableTeamBalance	= CreateConVar("ph_enable_teambalance", "1", CVAR_SERVER_ONLY_NO_NOTIFY, "Enable Team Balance during round restart?")
-PHX.CVAR.EnableNewChat		= CreateConVar("ph_use_new_chat", "0", CVAR_SERVER_ONLY, "REQUIRE MAP RESTART!\nUse new (temporary) chat box? This will replace default chatbox and prevent new HUD overlap.",0,1)
-PHX.CVAR.NewChatPosSubstract = CreateConVar("ph_new_chat_pos_sub", "50", CVAR_SERVER_ONLY_NO_NOTIFY, "Move by substracting chat position Y pixels high. Use (-) to move lower.")
-
-PHX.CVAR.AllowRespawnOnBlind			= CreateConVar("ph_allow_respawnonblind", "1", CVAR_SERVER_ONLY, "Allow fast respawn during blind time?")
-PHX.CVAR.AllowRespawnOnBlindTeam		= CreateConVar("ph_allow_respawnonblind_team_only", "0", CVAR_SERVER_ONLY, "If specified, what team should be allowed to respawn? 0 = ALL TEAMS, 1 = HUNTERS, 2 = PROPS", 0, 2 )
-PHX.CVAR.AllowSpectatorRespawnOnBlind	= CreateConVar("ph_allow_respawn_from_spectator", "1", CVAR_SERVER_ONLY, "Allow Spectators joining to any team and respawn during blind time?", 0, 1)
-PHX.CVAR.BlindRespawnTimePercent		= CreateConVar("ph_blindtime_respawn_percent", "0.75", CVAR_SERVER_ONLY, "How much time percentage to allow player Respawn during Blind time? Default is 0.75 (75%)", 0.30, 1.00)
-PHX.CVAR.AllowRespawnOnBlindBetweenTeams = CreateConVar("ph_allow_respawnonblind_teamchange", "0", CVAR_SERVER_ONLY, "Not recommended if allowed: Allow respawn during blind time FROM team changes (from props to hunters, vice versa).\nI don't recommend enabling this because players may able to use this to take advantage by sitting on Prop team everytime. Enable this ONLY if you know what you're doing.", 0, 1 )
-PHX.CVAR.AllowPickupProp	= CreateConVar("ph_allow_pickup_object", "3", CVAR_SERVER_ONLY, "Allow pickup objects? 0=No, 1=Hunters Only, 2=Props Only, 3=Everyone", 0, 3)
-
-PHX.CVAR.UseCustomMapVote	= CreateConVar("ph_use_custom_mapvote", "0", CVAR_SERVER_ONLY_NO_NOTIFY, "Use custom external Map votes system?", 0, 1)
-PHX.CVAR.CustomMapVoteCall	= CreateConVar("ph_custom_mv_func", "MapVote.Start()", CVAR_SERVER_HIDDEN, "If specified, what's the command? NOTE: Case Sensitive. Local variable will not passed to the given code.")
-
--- Taken from PH:E v16. This addition was made by Fafy
-PHX.CVAR.ForceJoinBalancedTeams = CreateConVar("ph_forcejoinbalancedteams", "0", CVAR_SERVER_ONLY, "Force players to even out teams upon joining? Setting 0 means do not force to join in balanced teams.", 0, 1)
-PHX.CVAR.SMGGrenadeCounts		= CreateConVar("ph_smggrenadecounts", "1", CVAR_SERVER_ONLY, "How many grenades for SMG1 served on spawn?", 1, 50)
-
--- Verbosity
-PHX.CVAR.BeVerbose			= CreateConVar("ph_print_verbose", "0", CVAR_SERVER_ONLY_NO_NOTIFY, "Developer Verbose. Some printed messages will only appear if this is enabled.", 0, 1)
 function PHX.VerboseMsg(text)
-	if ( PHX.CVAR.BeVerbose:GetBool() and text ) then
+	if ( PHX.GetVarBool("phvar_BeVerbose") and text ) then
 		print( tostring(text) )
 	end
 end
-
--- Callbacks
-cvars.AddChangeCallback("ph_use_new_chat", function(cvar,old,new)
-	print(cvar .. " -> has changed. Please be sure to Restart the map to take effect.")
-end, "ph_usenewchatsystem")
-
-cvars.AddChangeCallback("ph_forcejoinbalancedteams", function(cvar,old,new)
-	SetGlobalBool("bJoinBalancedTeam", tobool(new))
-end, "ph_joinbalanceteam")
 
 --Include Languages
 PHX.LANGUAGES = {}
@@ -146,7 +160,7 @@ include("sh_config.lua")
 include("sh_player.lua")
 
 -- Special Inclusion: ChatBox.
-if PHX.CVAR.EnableNewChat:GetBool() then 
+if PHX.GetVarBool("phvar_EnableNewChat") then
 	AddCSLuaFile("sh_chatbox.lua")
 	include("sh_chatbox.lua")
 end
@@ -156,11 +170,11 @@ if SERVER then
     AddCSLuaFile("sh_mapvote.lua")
     AddCSLuaFile("mapvote/cl_mapvote.lua")
 
-	include("sh_mapvote.lua")
+    include("sh_mapvote.lua")
     include("mapvote/sv_mapvote.lua")
     include("mapvote/rtv.lua")
 else
-	include("sh_mapvote.lua")
+    include("sh_mapvote.lua")
     include("mapvote/cl_mapvote.lua")
 end
 
@@ -191,7 +205,7 @@ end ]]
 GM.Help			= ""
 
 -- Fretta configuration
-GM.GameLength				= PHX.CVAR.GameTime:GetInt()
+GM.GameLength				= PHX.GetVarNum("phvar_GameTime")
 GM.AddFragsToTeamScore		= true
 GM.CanOnlySpectateOwnTeam 	= true
 GM.ValidSpectatorModes 		= { OBS_MODE_CHASE, OBS_MODE_IN_EYE, OBS_MODE_ROAMING }
@@ -201,8 +215,8 @@ GM.NoAutomaticSpawning		= true
 GM.NoNonPlayerPlayerDamage	= true
 GM.NoPlayerPlayerDamage 	= true
 GM.RoundBased				= true
-GM.RoundLimit				= PHX.CVAR.TotalRounds:GetInt()
-GM.RoundLength 				= PHX.CVAR.RoundTime:GetInt()
+GM.RoundLimit				= PHX.GetVarNum("phvar_TotalRounds")
+GM.RoundLength 				= PHX.GetVarNum("phvar_RoundTime")
 GM.RoundPreStartTime		= 0
 GM.SuicideString			= "dead" -- obsolete
 GM.TeamBased 				= true
@@ -230,7 +244,7 @@ end
 -- Check collisions
 function CheckPropCollision(entA, entB)
 	-- Disable prop on prop collisions
-	if !PHX.CVAR.PropCollide:GetBool() && (entA && entB && ((entA:IsPlayer() && entA:Team() == TEAM_PROPS && entB:IsValid() && entB:GetClass() == "ph_prop") || (entB:IsPlayer() && entB:Team() == TEAM_PROPS && entA:IsValid() && entA:GetClass() == "ph_prop"))) then
+	if !PHX.GetVarBool("phvar_PropCollide") && (entA && entB && ((entA:IsPlayer() && entA:Team() == TEAM_PROPS && entB:IsValid() && entB:GetClass() == "ph_prop") || (entB:IsPlayer() && entB:Team() == TEAM_PROPS && entA:IsValid() && entA:GetClass() == "ph_prop"))) then
 		return false
 	end
 	
